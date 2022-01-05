b7a1d8ebb881bb4986a58caed443cd5f0e6a825b
Katlego <40573286+Kpmahlaole@users.noreply.github.com>
2022-01-05 18:55:15 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <!doctype html>
 <html lang="en">
   <head>
@@ -11,3 +12,65 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   </body>
 </html>
+=======
+<?php require_once"inc/header.php"?>
+<link rel="stylesheet" type="text/css" href="css/style.css"> 
+<p></p>
+<div class="wrapper">
+    
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#captureapayment">
+  Capture a payment
+</button>
+
+<!-- captureapayment -->
+<div class="modal fade" id="captureapayment" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel"><strong>Capture Payment</strong></h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+  
+<form>
+
+<div class="input-group mb-3">
+  <span class="input-group-text" id="basic-addon3">Policy Number:</span>
+  <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">
+</div>
+
+<div class="input-group mb-3">
+  <span class="input-group-text" id="basic-addon3">Receipt Number: </span>
+  <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">
+</div>
+
+<div class="input-group mb-3">
+  <span class="input-group-text" id="basic-addon3">Receipt Date: </span>
+  <input type="date" class="form-control" id="basic-url" aria-describedby="basic-addon3">
+</div>
+
+<div class="input-group mb-3">
+  <span class="input-group-text" id="basic-addon3">Amount: </span>
+  <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">
+</div>
+
+<div class="input-group mb-3">
+  <span class="input-group-text" id="basic-addon3">Comments:</span>
+  <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">
+</div>
+
+
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-success">Process</button>
+      </div>
+    </div>
+  </div>
+</div>
+</form>
+
+<?php require_once"inc/footer.php"?>
+>>>>>>> Stashed changes
