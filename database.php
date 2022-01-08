0a8d60195d88e2bbc0086063ef6841588caa8173
Katlego <40573286+Kpmahlaole@users.noreply.github.com>
2022-01-08 10:17:14 +0200

    Update database.php

--- database.php
+++ database.php
@@ -1,8 +1,18 @@
 <?php
-$url='127.0.0.1:3306';
-$username='root';
-$password='';
-$conn=mysqli_connect($url,$username,$password,"foremost");
+//$url='127.0.0.1:3306';
+//$username='root';
+//$password='';
+//$conn=mysqli_connect($url,$username,$password,"foremost");
+//if(!$conn){
+// die('Could not Connect My Sql:' .mysql_error());
+//}
+?>
+
+<?php
+$url='sql6.freemysqlhosting.net';
+$username='sql6450823';
+$password='bq5uI3Pkbc';
+$conn=mysqli_connect($url,$username,$password,"sql6450823");
 if(!$conn){
  die('Could not Connect My Sql:' .mysql_error());
 }
