051877a4ecba8b973a46dae5653130674d5dee7f
Katlego <40573286+Kpmahlaole@users.noreply.github.com>
2022-01-08 07:12:17 +0200

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -54,15 +54,8 @@
 </div>
 
 
-<br>
+<div class="fixed-bottom">Foremost powered by Katweb Systems</div>
 
-
-
-
-        <div id="footer" class="p-2 bg-light fixed-bottom">
-            <p class="text-left">Copyright &copy; - IT Conference Attendance System <?php echo date('Y'); ?></p>
-
-        </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   </body>
