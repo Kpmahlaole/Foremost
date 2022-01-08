b46aaf2bfde70f771cc644dab07b9facc5bdb647
Katlego <40573286+Kpmahlaole@users.noreply.github.com>
2022-01-08 07:06:23 +0200

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -10,6 +10,7 @@
     <title>Hello, world!</title>
   </head>
   <body>
+  <div class="sticky-top">
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="#"><strong>FOREMOST</strong></a>
@@ -50,9 +51,18 @@
     </div>
   </div>
 </nav>
+</div>
+
+
 <br>
 
 
+
+
+        <div id="footer" class="p-2 bg-light fixed-bottom">
+            <p class="text-left">Copyright &copy; - IT Conference Attendance System <?php echo date('Y'); ?></p>
+
+        </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   </body>
