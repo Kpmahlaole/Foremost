e16da761f6f7cc42ff3877002245c67a37d3423a
Katlego <40573286+Kpmahlaole@users.noreply.github.com>
2022-07-20 20:28:41 +0200

    up

--- config.php
+++ config.php
@@ -1,15 +1,15 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-//define('db_server', 'localhost');
-//define('db_username', 'root');
-//define('db_password', '');
-//define('db_name', 'foremost');
+define('db_server', 'localhost');
+define('db_username', 'root');
+define('db_password', '');
+define('db_name', 'foremost');
 
-define('DB_SERVER', 'sql6.freemysqlhosting.net');
-define('DB_USERNAME', 'sql6450823');
-define('DB_PASSWORD', 'bq5uI3Pkbc');
-define('DB_NAME', 'sql6450823');
+//define('DB_SERVER', 'sql6.freemysqlhosting.net');
+//define('DB_USERNAME', 'sql6450823');
+//define('DB_PASSWORD', 'bq5uI3Pkbc');
+//define('DB_NAME', 'sql6450823');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(db_server, db_username, db_password, db_name);
